0b3ec0c76b680ba4cc0e4fff83ec6b6d266944d9
lucklymen <1217356552@qq.com>
2021-01-25 21:00:42 +0800

    链表方式实现栈

---
+++ code/栈/ListStack.js
@@ -0,0 +1,35 @@
+function ListStack() {
+  this.elements = new List()
+}
+
+ListStack.prototype.push = function(element) {
+  this.elements.append(element)
+}
+
+ListStack.prototype.pop = function() {
+  return this.elements.removeAt(this.size() - 1)
+}
+
+ListStack.prototype.peek = function() {
+  let peek = this.pop()
+  this.push(peek)
+  return peek
+}
+
+ListStack.prototype.isEmpty = function() {
+  return this.elements.isEmpty()
+}
+
+ListStack.prototype.clear = function() {
+  while(!this.isEmpty()) {
+    this.pop()
+  }
+}
+
+ListStack.prototype.size = function() {
+  return this.elements.length
+}
+
+ListStack.prototype.toString = function() {
+  return this.elements.toString()
+}
